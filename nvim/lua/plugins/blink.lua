11cd9927fd6475d0026cad7a13a8dd7563dd4bc5
stoicneko <anchor1618@outlook.com>
2025-04-21 16:10:16 +0800

    Add nvim, yazi, fish, tmux configs and Dotbot setup

---
+++ nvim/lua/plugins/blink.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "saghen/blink.cmp",
+    version = "*",
+    opts = {
+      keymap = {
+        ["<Tab>"] = { "select_next", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "fallback" },
+      },
+      completion = {
+        menu = { border = "single" },
+        documentation = { window = { border = "single" } },
+      },
+      signature = { window = { border = "single" } },
+    },
+  },
+}
---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "rafamadriz/friendly-snippets",
+    enabled = false,
+  },
+  {
+    "nvim-pack/nvim-spectre",
+    enabled = false,
+  },
+  {
+    "akinsho/bufferline.nvim",
+    keys = {
+      { "<S-h>", false },
+      { "<S-l>", false },
+    },
+  },
+}
