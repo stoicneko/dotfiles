922adbffe870b79c6860dc852f57c729120c84e2
stoicneko <anchor1618@outlook.com>
2026-03-14 07:39:13 -0700

    update

--- fish/conf.d/05-paths.fish
+++ fish/conf.d/05-paths.fish
@@ -1,8 +1,10 @@
 if type -q fish_add_path
     fish_add_path -m "$HOME/.local/bin"
     fish_add_path -m "$HOME/bin"
+    fish_add_path --append "$HOME/.npm-global/bin"
 else
     set -q PATH; or set PATH
     contains -- "$HOME/.local/bin" $PATH; or set -gx PATH "$HOME/.local/bin" $PATH
     contains -- "$HOME/bin" $PATH; or set -gx PATH "$HOME/bin" $PATH
+    contains -- "$HOME/.npm-global/bin" $PATH; or set -gx PATH $PATH "$HOME/.npm-global/bin"
 end
