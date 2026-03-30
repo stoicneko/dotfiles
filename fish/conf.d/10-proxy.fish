6b08aca28bdc526790a4aa4e3fa8ec7ea1992c39
stoicneko <anchor1618@outlook.com>
2026-03-30 11:48:15 +0800

    fix: use Docker bridge gateway for proxy to support minikube/containers
    localhost proxy is unreachable from inside Docker containers.
    Use 172.17.0.1 (docker0 gateway) instead, add lowercase variants
    and expanded NO_PROXY with private network ranges.

--- fish/conf.d/10-proxy.fish
+++ fish/conf.d/10-proxy.fish
@@ -1,7 +1,10 @@
 status is-interactive; or exit
 
-set -gx ALL_PROXY "http://localhost:7890"
+set -gx ALL_PROXY "http://172.17.0.1:7890"
 set -gx HTTP_PROXY $ALL_PROXY
 set -gx HTTPS_PROXY $ALL_PROXY
-set -gx NO_PROXY "localhost,127.0.0.1,::1"
+set -gx http_proxy $ALL_PROXY
+set -gx https_proxy $ALL_PROXY
+set -gx NO_PROXY "localhost,127.0.0.1,::1,192.168.0.0/16,10.0.0.0/8,172.16.0.0/12"
+set -gx no_proxy $NO_PROXY
 set -gx TWITTER_PROXY $ALL_PROXY
